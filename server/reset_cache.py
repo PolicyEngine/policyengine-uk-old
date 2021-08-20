546910a1ac35ca79b950498a0e79271ed60c1040
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-08-20 21:29:17 +0100

    Apply formatting

--- server/reset_cache.py
+++ server/reset_cache.py
@@ -3,4 +3,4 @@ from google.cloud import storage
 client = storage.Client()
 bucket = client.get_bucket("uk-policy-engine.appspot.com")
 for blob in bucket.list_blobs():
-	blob.delete()
+    blob.delete()
