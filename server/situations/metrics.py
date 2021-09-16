c4470dee9e44375a108aa81292fb78df73e374ae
Max Ghenis <mghenis@gmail.com>
2021-09-16 08:09:47 -0700

    Rename and change get_value(s) from lambda to def, separate variables as a variable

--- server/situations/metrics.py
+++ server/situations/metrics.py
@@ -11,32 +11,35 @@ def headline_figures(baseline: IndividualSim, reformed: IndividualSim) -> dict:
     :type reformed: IndividualSim
     :return: Dictionary of baseline and reformed sums for a set of variables
     """
-    getValue = lambda sim, name: float(np.array(sim.calc(name)).sum())
-    getValues = lambda name: {
-        "old": getValue(baseline, name),
-        "new": getValue(reformed, name),
-    }
-    return {
-        name: getValues(name)
-        for name in [
-            "tax",
-            "income_tax",
-            "basic_rate_earned_income_tax",
-            "higher_rate_earned_income_tax",
-            "add_rate_earned_income_tax",
-            "national_insurance",
-            "employee_NI_class_1",
-            "employer_NI_class_1",
-            "NI_class_2",
-            "NI_class_4",
-            "universal_credit",
-            "UC_maximum_amount",
-            "UC_income_reduction",
-            "benefits",
-            "child_benefit",
-            "household_net_income",
-            "total_income",
-            "gross_income",
-            "net_income",
-        ]
-    }
+
+    def get_value(sim, name):
+        return float(np.array(sim.calc(name)).sum())
+
+    def get_values(name):
+        return {
+            "old": get_value(baseline, name),
+            "new": get_value(reformed, name),
+        }
+
+    VARIABLES = [
+        "tax",
+        "income_tax",
+        "basic_rate_earned_income_tax",
+        "higher_rate_earned_income_tax",
+        "add_rate_earned_income_tax",
+        "national_insurance",
+        "employee_NI_class_1",
+        "employer_NI_class_1",
+        "NI_class_2",
+        "NI_class_4",
+        "universal_credit",
+        "UC_maximum_amount",
+        "UC_income_reduction",
+        "benefits",
+        "child_benefit",
+        "household_net_income",
+        "total_income",
+        "gross_income",
+        "net_income",
+    ]
+    return {name: get_values(name) for name in VARIABLES}
