7dc338efc15dfc1b4d11556525177b61e8197aa4
Max Ghenis <mghenis@gmail.com>
2021-10-01 07:45:45 -0700

    Rename utils/formatting.py to utils/charts.py

--- policy_engine_uk/populations/charts.py
+++ policy_engine_uk/populations/charts.py
@@ -1,5 +1,5 @@
 from policy_engine_uk.populations.metrics import poverty_rate, pct_change
-from policy_engine_uk.utils.formatting import format_fig, BLUE, GRAY, DARK_BLUE
+from policy_engine_uk.utils.charts import format_fig, BLUE, GRAY, DARK_BLUE
 import plotly.express as px
 from plotly.subplots import make_subplots
 import json
