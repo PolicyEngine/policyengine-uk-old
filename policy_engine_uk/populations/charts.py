8ee2e5677e592f9c7adffdc45dff5628a5772e14
Max Ghenis <mghenis@gmail.com>
2021-10-01 07:16:21 -0700

    Remove age chart

--- policy_engine_uk/populations/charts.py
+++ policy_engine_uk/populations/charts.py
@@ -190,35 +190,6 @@ def population_waterfall_chart(reform, labels, baseline, reformed):
     return json.loads(fig.to_json())
 
 
-def age_chart(baseline, reformed):
-    income = baseline.calc("household_net_income", map_to="person")
-    gain = reformed.calc("household_net_income", map_to="person") - income
-    values = gain.groupby(baseline.calc("age")).mean().rolling(3).median()
-    df = pd.DataFrame({"Age": values.index, "Change": values.values})
-    fig = (
-        format_fig(px.line(df, x="Age", y="Change"), show=False)
-        .update_layout(
-            title="Impact on net income by age",
-            xaxis_title="Age",
-            yaxis_title="Average change to net income",
-            yaxis_tickprefix="£",
-            showlegend=False,
-        )
-        .update_traces(marker_color=BLUE)
-    )
-    fig.add_shape(
-        type="line",
-        xref="paper",
-        yref="y",
-        x0=0,
-        y0=0,
-        x1=1,
-        y1=0,
-        line=dict(color="grey", width=1, dash="dash"),
-    )
-    return json.loads(fig.to_json())
-
-
 NAMES = (
     "Gain more than 5%",
     "Gain less than 5%",
