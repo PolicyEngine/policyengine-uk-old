a5083790fcae3b47c8bcb5aacc058ec0f0c7445c
Max Ghenis <mghenis@gmail.com>
2021-10-04 21:24:19 -0700

    Add docstrings for all populations/charts.py functions

--- policy_engine_uk/populations/charts.py
+++ policy_engine_uk/populations/charts.py
@@ -9,6 +9,15 @@ import pandas as pd
 
 
 def decile_chart(baseline: Microsimulation, reformed: Microsimulation) -> dict:
+    """Chart of average net effect of a reform by income decile.
+
+    :param baseline: Baseline microsimulation.
+    :type baseline: Microsimulation
+    :param reformed: Reform microsimulation.
+    :type reformed: Microsimulation
+    :return: Decile chart as a JSON representation of a Plotly chart.
+    :rtype: dict
+    """
     income = baseline.calc("household_net_income", map_to="person")
     equiv_income = baseline.calc("equiv_household_net_income", map_to="person")
     gain = reformed.calc("household_net_income", map_to="person") - income
@@ -36,12 +45,36 @@ def decile_chart(baseline: Microsimulation, reformed: Microsimulation) -> dict:
 def pov_chg(
     baseline: Microsimulation, reform: Microsimulation, criterion: str
 ) -> float:
+    """Calculate change in poverty rates.
+
+    :param baseline: Baseline simulation.
+    :type baseline: Microsimulation
+    :param reform: Reform simulation.
+    :type reform: Microsimulation
+    :param criterion: Filter for each simulation.
+    :type criterion: str
+    :return: Percentage (not percentage point) difference in poverty rates.
+    :rtype: float
+    """
     return pct_change(
         poverty_rate(baseline, criterion), poverty_rate(reform, criterion)
     )
 
 
 def poverty_chart(baseline: Microsimulation, reform: Microsimulation) -> dict:
+    """Chart of poverty impact by age group and overall.
+
+    :param baseline: Baseline microsimulation.
+    :type baseline: Microsimulation
+    :param reform: Reform microsimulation.
+    :type reform: Microsimulation
+    :return: JSON representation of Plotly chart with poverty impact for:
+        - Children (under 18)
+        - Working age adults (18 to State Pension age)
+        - Pensioners (State Pension age and above)
+        - Overall
+    :rtype: dict
+    """
     df = pd.DataFrame(
         {
             "group": ["Child", "Working-age", "Senior", "All"],
@@ -80,6 +113,15 @@ def poverty_chart(baseline: Microsimulation, reform: Microsimulation) -> dict:
 
 
 def spending(baseline: Microsimulation, reformed: Microsimulation) -> float:
+    """Budgetary impact of a reform (difference in net income).
+
+    :param baseline: Baseline microsimulation.
+    :type baseline: Microsimulation
+    :param reformed: Reform microsimulation.
+    :type reformed: Microsimulation
+    :return: Reform net income minus baseline net income.
+    :rtype: float
+    """
     return (
         reformed.calc("net_income").sum() - baseline.calc("net_income").sum()
     )
@@ -88,6 +130,15 @@ def spending(baseline: Microsimulation, reformed: Microsimulation) -> float:
 def get_partial_funding(
     reform: Microsimulation, baseline: Microsimulation, **kwargs
 ) -> list:
+    """Spending for each cumulative set of provisions in a reform.
+
+    :param reform: Reform simulation.
+    :type reform: Microsimulation
+    :param baseline: Baseline simulation.
+    :type baseline: Microsimulation
+    :return: List of cumulative spending for each reform provision.
+    :rtype: list
+    """
     expenditure = []
     for i in range(1, len(reform) + 1):
         expenditure += [
@@ -97,12 +148,28 @@ def get_partial_funding(
 
 
 def total_income(sim: Microsimulation) -> float:
+    """[summary]
+
+    :param sim: [description]
+    :type sim: Microsimulation
+    :return: [description]
+    :rtype: float
+    """
     return sim.calc("net_income").sum()
 
 
 def population_waterfall_chart(
     baseline: Microsimulation, reformed: Microsimulation
 ) -> dict:
+    """Waterfall chart showing a reform's budgetary impact.
+
+    :param baseline: Baseline simulation.
+    :type baseline: Microsimulation
+    :param reformed: Reform simulation.
+    :type reformed: Microsimulation
+    :return: JSON representation of Plotly chart with waterfall chart.
+    :rtype: dict
+    """
     return charts.waterfall_chart(baseline, reformed)
 
 
@@ -118,6 +185,15 @@ NAMES = (
 def intra_decile_graph_data(
     baseline: Microsimulation, reformed: Microsimulation
 ) -> pd.DataFrame:
+    """Data for the distribution of net income changes by decile and overall.
+
+    :param baseline: Baseline simulation.
+    :type baseline: Microsimulation
+    :param reformed: Reform simulation.
+    :type reformed: Microsimulation
+    :return: DataFrame with share of each decile experiencing each outcome.
+    :rtype: pd.DataFrame
+    """
     l = []
     income = baseline.calc("equiv_household_net_income", map_to="person")
     decile = income.decile_rank()
@@ -173,6 +249,17 @@ INTRA_DECILE_COLORS = (
 
 
 def intra_decile_label(fraction: float, decile: str, outcome: str) -> str:
+    """Label for a data point in the intra-decile chart for hovercards.
+
+    :param fraction: Share of the decile experiencing the outcome.
+    :type fraction: float
+    :param decile: Decile number as a string, or "All".
+    :type decile: str
+    :param outcome: Outcome, e.g. "Gain more than 5%".
+    :type outcome: str
+    :return: String representation of the hovercard label.
+    :rtype: str
+    """
     res = "{:.0%}".format(fraction) + " of "  # x% of
     if decile == "All":
         res += "all people "
@@ -185,6 +272,13 @@ def intra_decile_label(fraction: float, decile: str, outcome: str) -> str:
 
 
 def single_intra_decile_graph(df: pd.DataFrame) -> go.Figure:
+    """Single intra-decile graph, for either by-decile or overall.
+
+    :param df: DataFrame with intra-decile or intra-overall data.
+    :type df: pd.DataFrame
+    :return: Plotly bar chart.
+    :rtype: go.Figure
+    """
     fig = px.bar(
         df,
         x="fraction",
@@ -201,6 +295,15 @@ def single_intra_decile_graph(df: pd.DataFrame) -> go.Figure:
 def intra_decile_chart(
     baseline: Microsimulation, reformed: Microsimulation
 ) -> dict:
+    """Full intra-decile chart, including a top bar for overall.
+
+    :param baseline: Baseline simulation.
+    :type baseline: Microsimulation
+    :param reformed: Reform simulation.
+    :type reformed: Microsimulation
+    :return: JSON representation of Plotly intra-decile chart.
+    :rtype: dict
+    """
     df = intra_decile_graph_data(baseline, reformed)
     df["hover"] = df.apply(
         lambda x: intra_decile_label(x.fraction, x.decile, x.outcome), axis=1
